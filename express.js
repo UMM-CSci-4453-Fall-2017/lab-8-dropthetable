2614e7dca97cc68bfbbb1a16ec5d3e0f8309c809
GLOBBIMUS <striker1342566@gmail.com>
2017-11-13 16:07:15 -0600

    Create "/transaction" end point, and modify "/click"

--- express.js
+++ express.js
@@ -5,19 +5,17 @@ var commandInput = myArgs[0];
 GetButtons=require('./getButtons.js');
 var express=require('express'),
 mysql=require('mysql'),
-credentials=require('./credentials.json'),
+
 app = express(),
 port = process.env.PORT || 1337;
-
 
 var buttons;
 
 app.use(express.static(__dirname + '/public'));
 
-GetButtons.receivedRecords(commandInput)
-.then(function(result){buttons = result})
-.then(GetButtons.releaseButtons)
-.then(function(){console.log("Starting server")});
+GetButtons.receivedButtons(commandInput)
+.then(function(result){buttons = result});
+
 
 app.get("/buttons",function(req,res){ // handles the /buttons API
   res.send(buttons);
@@ -25,15 +23,19 @@ app.get("/buttons",function(req,res){ // handles the /buttons API
 
 app.get("/click",function(req,res){
   var id = req.param('id');
-  var sql = 'YOUR SQL HERE'
-  console.log("Attempting sql ->"+sql+"<-");
-
-  connection.query(sql,(function(res){return function(err,rows,fields){
-     if(err){console.log("We have an insertion error:");
-             console.log(err);}
-     res.send(err); // Let the upstream guy know how it went
-  }})(res));
+  GetButtons.click(id, commandInput)
+  .then(function(){
+       res.send();
+     });
 });
+
+app.get("/transactions", function(req, res){
+  GetButtons.getTotalPrice(commandInput)
+  .then(function(result){
+    res.send(result);
+  });
+});
+
 // Your other API handlers go here!
 
 app.listen(port);
