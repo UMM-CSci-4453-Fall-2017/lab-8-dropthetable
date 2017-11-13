11af3f60f6d1aa98d6f617c219388dfe2d6ea49e
GLOBBIMUS <striker1342566@gmail.com>
2017-11-13 16:06:06 -0600

    Fix asynchronous promises

--- getButtons.js
+++ getButtons.js
@@ -1,4 +1,4 @@
-var credentials = require('./credentials.json');
+var credentials=require('./credentials.json');
 
 var mysql=require("mysql");
 var Promise = require('bluebird');
@@ -13,36 +13,73 @@ var connection = mysql.createConnection(credentials);
 var pool=mysql.createPool(credentials);
 
 var getConnection=function(){
-    return pool.getConnectionAsync().disposer(
-        function(connection){return connection.release();}
-    );
+  return pool.getConnectionAsync().disposer(
+    function(connection){return connection.release();}
+  );
 };
 
 var query=function(command){ //SQL comes in and a promise comes out.
-    return using(getConnection(),function(connection){
-        return connection.queryAsync(command);
-    });
+  return using(getConnection(),function(connection){
+    return connection.queryAsync(command);
+  });
 };
 
 var endPool=function(){
-    console.log("End Connection to DB");
-    pool.end(function(err){});
+  console.log("End Connection to DB");
+  pool.end(function(err){});
 }
 
-var useDB = function(db){
-    var sql  = "use "+db+";";
-    return query(mysql.format(sql));
+
+var selectButtonRecord = function(db){
+  var sql  = "select * from "+db+".till_buttons;"
+  console.log(sql);
+  return query(mysql.format(sql));
 }
 
-var selectButtonRecord = function(){
-    var sql  = "select * from till_buttons;"
-    return query(mysql.format(sql));
+var receivedButtons = function(db) {
+  return receiveRecords = selectButtonRecord(db);
 }
 
-var receivedRecords = function(db) {
-    return receiveRecords = useDB(db)
-    .then(selectButtonRecord)
+
+var checkSupply = function(itemID, db){
+  var sql = "select * from "+db+".supply where itemID = "+itemID+";";
+  console.log(sql);
+  var recordsExists = false;
+  var price;
+  return query(mysql.format(sql))
+  .then(function(result){
+    if(result.length > 0){
+      price = result[0].price;
+      recordsExists = true;
+      //checkTransaction(itemID, price, db);
+    }else{
+      console.log("No item in supply with itemID "+itemID);
+    }
+  })
+  .then(function() {
+    if(recordsExists) {
+      checkTransaction(itemID, price, db);
+    }else{
+      console.log("No item in supply with itemID "+itemID);
+    }
+  });
 }
 
-exports.receivedRecords = receivedRecords;
+
+var checkTransaction = function(itemID, price, db){
+  //var sql = "select * from "+db+".transactions where itemID = "+itemID+";";
+  var sql = "INSERT INTO " + db + ".transactions VALUES (" + itemID + ", " + 1 + ", " + price + ") ON DUPLICATE KEY UPDATE totalPrice = (quantity + 1)* " + price + " , quantity  = quantity+1;";
+  query(mysql.format(sql));
+}
+
+
+var getTotalPrice = function(db){
+  var sql = "select (select itemName from "+db+".supply where "+db+".supply.itemID = "+db+".transactions.itemID) as itemName, quantity, totalPrice from "+db+".transactions;";
+  return query(mysql.format(sql));
+}
+
+
+exports.receivedButtons = receivedButtons;
 exports.releaseButtons = endPool;
+exports.click = checkSupply;
+exports.getTotalPrice = getTotalPrice;
